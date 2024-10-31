394311f03847455ac44d78e882ba87dabc4a1342
carsoncrum <carsoncrum07@gmail.com>
2024-10-31 16:59:42 +0000

    Coded

--- main.cpp
+++ main.cpp
@@ -1,44 +1,72 @@
 // Calculating Shipping Charges
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 
 {
-  double weight, distance;
-  double rate, charges;
+  int choice;
+  double radius, length, width, base, height;
 
+  cout << "Geometry Calculator" << endl;
+  cout << "1. Calculate the Area of a Circle" << endl;
+  cout << "2. Calculate the Area of a Rectangle" << endl;
+  cout << "3. Calculate the Area of a Triangle" << endl;
+  cout << "4. Quit" << endl;
   do {
-    cout << "Enter the weight of the package (in kilograms): ";
-    cin >> weight;
-    if (weight <= 0 || weight > 20) {
-      cout << "Invalid weight. Please enter a weight between 0 and 20 kg." << endl;
+    cout << "Enter your choice (1–4): ";
+    cin >> choice;
+    
+    if (choice < 1 || choice > 4) {
+      cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
+    } else {
+      switch (choice) {
+        case 1:
+          cout << "Enter the radius of the circle: ";
+          cin >> radius;
+          if (radius < 0) {
+            cout << "Invalid radius. Please enter a non-negative value." << endl;
+          } else {
+            double area = 3.14159 * radius * radius;
+            cout << "The area of the circle is: " << area << endl;
+          }
+          break;
+        case 2: 
+          cout << "Enter the length of the rectangle: ";
+          cin >> length;
+          if (length < 0) {
+            cout << "Invalid length. Please enter a non-negative value." << endl;
+          } else {
+            cout << "Enter the width of the rectangle: ";
+            cin >> width;
+            if (width < 0) {
+              cout << "Invalid width. Please enter a non-negative value." << endl;
+            } else {
+              double area = length * width;
+              cout << "The area of the rectangle is: " << area << endl;
+            }
+          }
+          break;
+        case 3:
+          cout << "Enter the base of the triangle: ";
+          cin >> base;
+          if (base < 0) {
+            cout << "Invalid base. Please enter a non-negative value." << endl;
+          } else {
+            cout << "Enter the height of the triangle: ";
+            cin >> height;
+            if (height < 0) {
+              cout << "Invalid height. Please enter a non-negative value." << endl;
+            } else {
+              double area = base * height * 0.5;
+              cout << "The area of the triangle is: " << area << endl;
+            }
+          }
+          break;
+      }
     }
-  } while (weight <= 0 || weight > 20);
-
-  do {
-    cout << "Enter the shipping distance (in miles): ";
-    cin >> distance;
-    if (distance < 10 || distance > 3000) {
-      cout << "Invalid distance. Please enter a distance between 10 and 3000 miles." << endl;
-    }
-  } while (distance < 10 || distance > 3000);
-
-  if (weight <= 2) {
-    rate = 1.10;
-  } else if (weight <= 6) {
-    rate = 2.20;
-  } else if (weight <= 10) {
-    rate = 3.70;
-  } else {
-    rate = 4.80;
-  }
-  
-  charges = rate * ceil(distance / 500.0); // ceil rounds up to the nearest integer
-
-  cout << "Total shipping charges: $" << charges << endl;
-
+  } while (choice != 4);
+  cout << "Goodbye!" << endl;
   
   return 0;
 }

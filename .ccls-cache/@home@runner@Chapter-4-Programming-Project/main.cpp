2c90f89a15fdd42d50f0b022fd58ddc0f10be550
carsoncrum <carsoncrum07@gmail.com>
2024-10-31 16:41:37 +0000

    Coded

--- .ccls-cache/@home@runner@Chapter-4-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-4-Programming-Project/main.cpp
@@ -1,3 +1,44 @@
+// Calculating Days in a Month
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main()
+
+{
+  int month, year;
+
+  do {
+    cout << "Enter a month (1–12): ";
+    cin >> month;
+    if (month < 1 || month > 12) {
+      cout << "Invalid month. Please enter a month between 1 and 12." << endl;
+    }
+  } while (month < 1 || month > 12);
+
+  cout << "Enter a year: ";
+  cin >> year;
+  bool isLeapYear = false;
+  if (year % 100 == 0) {
+    isLeapYear = (year % 400 == 0);
+  } else {
+    isLeapYear = (year % 4 == 0);
+  }
+  
+  int daysInMonth;
+  switch (month) {
+    case 2:
+      daysInMonth = (isLeapYear) ? 29 : 28;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      daysInMonth = 30;
+      break;
+    default:
+      daysInMonth = 31;
+  }
+cout << daysInMonth << " days " << endl;
+  
+  return 0;
+}
